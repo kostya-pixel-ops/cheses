9326fc9a5ff57e413af0c9b9b26857e60a1fc463
RedFox499 <igrok.inbox@gmail.com>
2024-12-01 21:51:53 +0500

    фигня получилась

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: [],
   theme: {
+    screens: {
+      'ltl': '360px',
+      // => @media (min-width: 360px) { ... }
+      'stl': '410px',
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1200px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
+    
   },
   plugins: [],
 }
